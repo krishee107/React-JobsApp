68824a323185b2c8aabc1514ffe5b61f3b62fecc
Cristina Ferrer <krishee107@gmail.com>
2023-04-24 11:24:43 +0200

    Estilo básico filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,6 +2,38 @@ import './Filter.css'
 
 export const Filter = () => {
   return (
-    <div>Filter</div>
+    <div className="filter">
+      <div className="jornada">
+        <label>
+          <input type="checkbox" name="fulltime" id="fulltime" />
+          Full  time
+        </label>
+      </div>
+
+      <div className="location">
+        <h3 className='location_title'>LOCATION</h3>
+        <input type="text" name="location" id="location" placeholder='City, state, zip code or country ' />
+      </div>
+
+      <div className="cityList">
+        <label>
+          <input type="radio" name="radio" className="city" value="london" />
+          London
+        </label>
+        <label>
+          <input type="radio" name="radio" className="city" value="amsterdam" />
+          Amsterdam
+        </label>
+        <label>
+          <input type="radio" name="radio" className="city" value="new york" />
+          New York
+        </label>
+        <label>
+          <input type="radio" name="radio" className="city" value="berlin" />
+          Berlin
+        </label>
+
+      </div>
+    </div>
   )
 }
