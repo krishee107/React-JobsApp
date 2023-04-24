87cf771a5c8f2a44b07fc9cc2b1d0a990760826a
Cristina Ferrer <krishee107@gmail.com>
2023-04-24 10:36:29 +0200

    Icono para SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,10 @@ import './SearchBar.css'
 export const SearchBar = () => {
   return (
     <div className="header">
-      <input type="text" name="search" id="search" placeholder='Title, companies, expertise or benefits' />
+      <div className='inputGroup'>
+        <input type="text" name="search" id="search" placeholder='Title, companies, expertise or benefits' />
+        <i class="fa-solid fa-suitcase"></i>
+      </div>
       <button>Search</button>
     </div>
   )
