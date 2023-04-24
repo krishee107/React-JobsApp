e0318223e3afbc5dd267421d322862b8bde19104
Cristina Ferrer <krishee107@gmail.com>
2023-04-24 11:47:24 +0200

    Fix style del content para que sea más responsive y estructura base de los jobs

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -2,6 +2,77 @@ import './JobList.css'
 
 export const JobList = () => {
   return (
-    <div>JobList</div>
+    <div className="jobList">
+
+      <div className="job">
+        <div className="job_img">
+          <img src="https://kasisto.com/wp-content/themes/kasisto/img/client-logo.png" alt="Job logo" />
+        </div>
+
+        <div className="job_info">
+          <div className="job_enterprise">Kasisto</div>
+          <div className="job_title">Front-end Software Engineer</div>
+          <div className="time">Full time</div>
+        </div>
+
+        <div className="job_status">
+          <div className="city">
+            <i class="fa-solid fa-earth-americas"></i>
+            New york
+          </div>
+          <div className="ago">
+            <i class="fa-regular fa-clock"></i>
+            5 days ago
+          </div>
+        </div>
+      </div>
+
+      <div className="job">
+        <div className="job_img">
+          <img src="https://kasisto.com/wp-content/themes/kasisto/img/client-logo.png" alt="Job logo" />
+        </div>
+
+        <div className="job_info">
+          <div className="job_enterprise">Kasisto</div>
+          <div className="job_title">Front-end Software Engineer</div>
+          <div className="time">Full time</div>
+        </div>
+
+        <div className="job_status">
+          <div className="city">
+            <i class="fa-solid fa-earth-americas"></i>
+            New york
+          </div>
+          <div className="ago">
+            <i class="fa-regular fa-clock"></i>
+            5 days ago
+          </div>
+        </div>
+      </div>
+
+      <div className="job">
+        <div className="job_img">
+          <img src="https://kasisto.com/wp-content/themes/kasisto/img/client-logo.png" alt="Job logo" />
+        </div>
+
+        <div className="job_info">
+          <div className="job_enterprise">Kasisto</div>
+          <div className="job_title">Front-end Software Engineer</div>
+          <div className="time">Full time</div>
+        </div>
+
+        <div className="job_status">
+          <div className="city">
+            <i class="fa-solid fa-earth-americas"></i>
+            New york
+          </div>
+          <div className="ago">
+            <i class="fa-regular fa-clock"></i>
+            5 days ago
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
