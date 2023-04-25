a92f8a9d4e56e9fffc4cc64d7e7380f1f7cfada9
Cristina Ferrer <krishee107@gmail.com>
2023-04-25 15:11:20 +0200

    Guardar el login (guardar pese a f5)

--- src/applications/router.jsx
+++ src/applications/router.jsx
@@ -1,19 +1,37 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { auth, onAuthStateChanged } from '../firebase/firebase';
+import { useDispatch } from 'react-redux';
+
+import { login } from '../actions/auth';
+
 import { Home } from '../pages/Home/Home';
 import { Profile } from '../pages/Profile/Profile';
 import { JobDetails } from '../components/JobDetails/JobDetails';
 import { Login } from '../components/Login/Login';
 import { Register } from '../components/Register/Register';
-import { useEffect, useState } from 'react';
 
 const Router = () => {
+    const dispatch = useDispatch();
     const [checking, setChecking] = useState(true)
     const [isLoggedIn, setIsLoggedIn] = useState(false)
 
     useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+                setIsLoggedIn(true)
+            } else {
+                setIsLoggedIn(false)
+            }
+            setChecking(false)
+        });
+    }, [dispatch, checking, isLoggedIn])
 
-    }, [])
-
+    if (checking) {
+        return <h3>cargando...</h3>
+    }
 
     return (
 
@@ -23,6 +41,7 @@ const Router = () => {
         </Routes>
     )
 };
+
 
 export default Router;
 
