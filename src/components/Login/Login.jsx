8ca9e4248651f1bf43b66cb77409bd509b3d0f13
Cristina Ferrer <krishee107@gmail.com>
2023-04-25 14:55:34 +0200

    Eliminado codigo comentado sin uso

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react'
-//import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 import './login.css'
 import { startGoogleAuth } from '../../actions/auth';
 
 export const Login = () => {
-  //const [isRegistrando, setIsRegistrando] = useState(false)
+
   const dispatch = useDispatch();
   const handleGoogleAuth = () => {
     dispatch(startGoogleAuth())
@@ -19,38 +18,3 @@ export const Login = () => {
 
   )
 }
-
-/*
-const submitHandler = (e) => {
-  e.preventDefault();
-  const correo = e.target.correo.value;
-  const password = e.target.password.value;
-
-  // Firebase
-  const auth = getAuth();
-  createUserWithEmailAndPassword(auth, correo, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorcode + ": " + errorMessage)
-      alert(errorMessage)
-    });
-
-}*/
-
-/* 
-      <form onSubmit={handleGoogleAuth}>
-        <h1>{isRegistrando ? "Regístrate" : "Inicia sesión"}</h1>
-        <input type="email" placeholder='Email' name='correo' />
-        <input type="password" placeholder='Password' name='password' />
-        <button type="submit">{isRegistrando ? "Regístrate" : "Inicia sesión"}</button>
-      </form>
-
-      <button onClick={() => setIsRegistrando(!isRegistrando)}>
-        {isRegistrando ? "¿Ya tienes cuenta? ¡Inicia sesión!" : "¿No tienes cuenta? ¡Regístrate!"}
-      </button>
-*/
