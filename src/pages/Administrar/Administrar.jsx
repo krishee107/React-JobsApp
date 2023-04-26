14871d8fbdffe5673c566b7451f8a58a204d6745
Cristina Ferrer <krishee107@gmail.com>
2023-04-26 15:22:11 +0200

    Listar las ofertas en el panel de administrador

--- src/pages/Administrar/Administrar.jsx
+++ src/pages/Administrar/Administrar.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Administrar.css'
 
+import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
+import { app } from '../../firebase/firebase';
+import { Link } from 'react-router-dom';
+import { Job } from '../../components/Job/Job';
+
+
 export const Administrar = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const dbFirestore = getFirestore(app);
+    const jobsCollectionRef = collection(dbFirestore, 'ofertas');
+
+    const unsubscribe = onSnapshot(jobsCollectionRef, snapshot => {
+      setJobs(snapshot.docs.map(doc => ({
+        id: doc.id,
+        creacion: doc.data().creacion,
+        ...doc.data()
+      })));
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div>Administrar</div>
+    <div className="adminPanel">
+      <div className="jobList">
+        {jobs.map(job => {
+          return (
+            <Link to={`/jobDetail/${job.id}`} key={job.id} style={{ textDecoration: 'none' }}>
+              <Job job={job} />
+            </Link >
+          )
+        })
+        }
+      </div>
+
+    </div>
   )
 }
