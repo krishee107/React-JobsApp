6533210707baaa1ca8da45dd016a9616ae9dca00
Cristina Ferrer <krishee107@gmail.com>
2023-04-26 12:36:17 +0200

    Mostrar el texto de la oferta procesado por párrafos

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -8,8 +8,10 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 export const JobDetails = () => {
   const { id } = useParams();
   const [job, setJob] = useState(null);
+  const [texto, setTexto] = useState(null)
 
   useEffect(() => {
+    console.log("INICIO: " + job)
     const db = getFirestore();
     const jobRef = doc(db, "ofertas", id);
     const getJob = async () => {
@@ -24,100 +26,91 @@ export const JobDetails = () => {
     console.log(job)
   }, [id]);
 
+  useEffect(() => {
+    if (job) {
+      arreglarTexto();
+    }
+  }, [job]);
+
+  const arreglarTexto = () => {
+    const parrafos = job.texto.split("\n\n");
+    let texto_con_etiquetas = "";
+    for (let parrafo of parrafos) {
+      texto_con_etiquetas += `<p>${parrafo}</p>`;
+    }
+    setTexto(texto_con_etiquetas);
+    console.log(texto_con_etiquetas);
+  };
 
   return (
-    <div className="jobDetails">
-      <div className="jobOptions">
-        <Link to="/">
-          <ArrowBackIcon />
-          Back to search
-        </Link>
+    <div >
+      {job ?
+        <div className="jobDetails">
+          <div className="jobOptions">
+            <Link to="/">
+              <ArrowBackIcon />
+              Back to search
+            </Link>
 
-        <div className="howTo">
-          <h3>HOW TO APPLY</h3>
-          <p>
-            Please email a copy of your resume and online portfolio to <span>example@job.com</span>  & cc <span>otherexample@joboffer.com</span>
-          </p>
-        </div>
-      </div>
+            <div className="howTo">
+              <h3>HOW TO APPLY</h3>
+              <p>
+                Please email a copy of your resume and online portfolio to <span>example@job.com</span>  & cc <span>otherexample@joboffer.com</span>
+              </p>
+            </div>
+          </div>
 
-      <div className="jobInfo">
+          <div className="jobInfo">
 
-        <div className="jobHeader">
-          <div className="jobTitle">
-            {job.titulo}
-            <div>
-              {
-                job.time == 'fullTime' ?
-                  <div className="time">Full time</div>
+            <div className="jobHeader">
+              <div className="jobTitle">
+                {job.titulo}
+                <div>
+                  {
+                    job.time == 'fullTime' ?
+                      <div className="time">Full time</div>
+                      :
+                      <div className="time">Part time</div>
+                  }
+                </div>
+              </div>
+              <div className="job_status">
+                <div className="ago">
+                  <i className="fa-regular fa-clock"></i>
+                  5 days ago
+                </div>
+              </div>
+            </div>
+
+            <div className="job_profile">
+              <div className="job_profile_img">
+                {job.urlLogo != '' ?
+                  <img src={job.urlLogo} />
                   :
-                  <div className="time">Part time</div>
+                  <img src="https://gigr.com/images/png/default_logo.png" alt="Job logo" />
+                }
+              </div>
+              <div className="job_profile_name">{job.city}</div>
+              <div className="job_profile_city">
+                <i className="fa-solid fa-earth-americas"></i>
+                New york
+              </div>
+            </div>
+
+            <div className="job_text">
+              {texto ?
+                <div dangerouslySetInnerHTML={{ __html: texto }}></div>
+                :
+                <div>Ha ocurrido un error al mostrar el texto o esta oferta no tiene ningún texto.</div>
               }
+
             </div>
-          </div>
-          <div className="job_status">
-            <div className="ago">
-              <i className="fa-regular fa-clock"></i>
-              5 days ago
-            </div>
+
           </div>
         </div>
-
-        <div className="job_profile">
-          <div className="job_profile_img">
-            {job.urlLogo != '' ?
-              <img src={job.urlLogo} />
-              :
-              <img src="https://gigr.com/images/png/default_logo.png" alt="Job logo" />
-            }
-          </div>
-          <div className="job_profile_name">{job.city}</div>
-          <div className="job_profile_city">
-            <i className="fa-solid fa-earth-americas"></i>
-            New york
-          </div>
-        </div>
-
-        <div className="job_text">
-          <p>
-            Humanizing Digital Experiences®
-          </p>
-          <p>
-            Kasisto’s Digital Experience Platform, KAI, is designed for financial institutions to deliver the industry’s most amazing Conversational AI powered intelligent virtual assistants to their customers. KAI is open and extensible, and also fluent in the language of banking and finance. From simple retail transactions to the complex demands of corporate banks and wealth management, financial institutions can deliver meaningful digital interactions with KAI that help build their digital brand.
-          </p>
-          <p>
-            Financial institutions around the world use KAI, including DBS Bank, J.P. Morgan, Mastercard, Standard Chartered, TD Bank, and Manulife Bank among others. They chose KAI for its proven track record to drive business results while improving customer experiences. The platform is used by millions of consumers around the world, all the time, across multiple channels, in different languages, and is optimized for performance, scalability, security, and compliance.
-          </p>
-          <p>
-            This position
-          </p>
-          <p>
-            We are looking for a Full-Stack, client side software engineer to help build and integrate responsive chat interfaces, analytics dashboards and reporting tools.
-          </p>
-
-          <p>What you’ll be doing</p>
-          <p>
-            Working closely with clients and internal engineering, product and design teams to gather requirements Building and integrating front-end applications with CSS, HTML, Javascript, jQuery, Vue.js, Webpack, Handlebars.js, LESS, Backbone, Python, Django and Java Working to improve user experience and functionality for tools Writing testable code utilizing common front-end unit and BDD testing frameworks What you need for this position
-          </p>
-          <p>
-            3+ years in client-side web development with CSS, HTML, Javascript and jQuery Proven, full-stack front-end experience using Python and Django Other Modern Web Framework(s) experience is a plus (React, Vue, Angular, Ember) Experience working collaboratively to build scalable, modular, production software in an Agile environment Experience using RESTful json services Node.js and API development familiarity is plus D3.js is a plus
-          </p>
-          <p>
-            What we offer:
-          </p>
-          <p>
-            Competitive compensation package Ground floor opportunity within rapidly growing tech startup Great collaborative team environment Full Benefits Fun perks
-          </p>
-          <p>
-            Location - NYC, Flatiron District
-          </p>
-          <p>
-            We welcome your cover letter with a description of your previous complete experience and your resume. Applicants must be authorized to work in the US as we are unable to sponsor. Kasisto is an equal opportunity employer.
-          </p>
-
-        </div>
-
-      </div>
+        :
+        <div>Cargando...</div>
+      }
     </div >
   )
 }
